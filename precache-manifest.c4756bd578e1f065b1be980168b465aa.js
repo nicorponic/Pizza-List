7b51a280b56bec1b92f9a613922ce6c9ff575264
nicorponic <n1co_frago@hotmail.com>
2019-01-24 18:15:25 +0100

    Updates

---
+++ precache-manifest.c4756bd578e1f065b1be980168b465aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da1539e221b99d448137",
+    "url": "/Pizza-List/static/css/main.328b90dd.chunk.css"
+  },
+  {
+    "revision": "da1539e221b99d448137",
+    "url": "/Pizza-List/static/js/main.da1539e2.chunk.js"
+  },
+  {
+    "revision": "8e00404e9099a931a439",
+    "url": "/Pizza-List/static/js/1.8e00404e.chunk.js"
+  },
+  {
+    "revision": "72fe2412aa3960972d26",
+    "url": "/Pizza-List/static/js/runtime~main.72fe2412.js"
+  },
+  {
+    "revision": "36030b5f72d1c72d49ae9ae6c6a64a23",
+    "url": "/Pizza-List/index.html"
+  }
+];
