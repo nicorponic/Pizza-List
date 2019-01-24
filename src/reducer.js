ce4812c62fe02b8f48eb93c849d45f3d694650e0
nicorponic <n1co_frago@hotmail.com>
2019-01-24 16:05:51 +0100

    Add initialStateData

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,28 @@
 const reducer = (state = [], action = {}) => {
   switch (action.type) {
     default:
-      return state;
+      const initialState = [
+        {
+          id: 1,
+          name: "Pizza Margarita",
+          description: "Minimalism is key!",
+          ingredients: ["tomato", "mozarella", "basil"]
+        },
+        {
+          id: 2,
+          name: "Pizza Napoletana",
+          description: "The mafia knows whats best for you",
+          ingredients: ["tomato", "mozarella"]
+        },
+        {
+          id: 3,
+          name: "Pizza Bianca",
+          description: "Did somebody say oil?",
+          ingredients: ["olive oil", "salt", "rosemary"]
+        }
+      ];
+      return initialState;
   }
 };
 
 export default reducer;
-
